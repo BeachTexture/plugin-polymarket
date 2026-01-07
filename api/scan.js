280c710ba9bc63642e2e368f6e5a06d60c3bd611
Klingon Droid <199229494+klingon-droid@users.noreply.github.com>
2026-01-07 18:47:03 +0100

    Fix API export for Vercel - use CommonJS module.exports

--- api/scan.js
+++ api/scan.js
@@ -110,7 +110,7 @@ async function analyzeMarket(market) {
   };
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
