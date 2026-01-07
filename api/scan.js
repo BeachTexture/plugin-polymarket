5a1077acdeba39294b2041ced74d3403f3d57086
Klingon Droid <199229494+klingon-droid@users.noreply.github.com>
2026-01-07 18:54:26 +0100

    Use ES module export for Vercel API function

--- api/scan.js
+++ api/scan.js
@@ -110,7 +110,7 @@ async function analyzeMarket(market) {
   };
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
